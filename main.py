e61edb78c65acc6818d99e924a7800c42e421033
Chandra shekhar arya <66366710+shekhar613@users.noreply.github.com>
2022-02-16 16:04:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-'''
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-'''
-# if input number cannot be expresed in the form of 4^a(8b+7)
-
 num = int(input("Enter a number"))
 
 def threesquares(m):
